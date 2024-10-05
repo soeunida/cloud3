85c685f8c7bdad1a357f341cdfb1089454a900ba
hayo10 <hayoung10.lim@gmail.com>
2024-10-05 23:37:03 +0900

    hide 추가

---
+++ model_flex_hide.py
@@ -0,0 +1,404 @@
+from typing import List, Optional, Tuple, Union
+from dataclasses import dataclass
+import torch
+import torch.utils.checkpoint
+from torch import nn
+from transformers import PreTrainedModel, Phi3Config
+from transformers.utils import ModelOutput
+from safetensors import safe_open
+import json
+from hf_ref import (
+    Phi3RMSNorm,
+    Phi3DecoderLayer,
+    NewPhi3Config
+)
+
+import time
+pre_weight_map = {}
+file_num = 1
+tensor_dict = {}
+    
+    
+
+def _prepare_4d_causal_attention_mask_with_cache_position(
+    attention_mask: torch.Tensor,
+    sequence_length: int,
+    target_length: int,
+    dtype: torch.dtype,
+    device: torch.device,
+    min_dtype: int,
+    cache_position: torch.Tensor,
+    batch_size: int,
+):
+    if attention_mask is not None and attention_mask.dim() == 4:
+        
+        causal_mask = attention_mask
+    else:
+        causal_mask = torch.full((sequence_length, target_length), fill_value=min_dtype, dtype=dtype, device=device)
+        if sequence_length != 1:
+            causal_mask = torch.triu(causal_mask, diagonal=1)
+        causal_mask *= torch.arange(target_length, device=device) > cache_position.reshape(-1, 1)
+        causal_mask = causal_mask[None, None, :, :].expand(batch_size, 1, -1, -1)
+        if attention_mask is not None:
+            causal_mask = causal_mask.clone()  # copy to contiguous memory for in-place edit
+            mask_length = attention_mask.shape[-1]
+            padding_mask = causal_mask[:, :, :, :mask_length] + attention_mask[:, None, None, :]
+            padding_mask = padding_mask == 0
+            causal_mask[:, :, :, :mask_length] = causal_mask[:, :, :, :mask_length].masked_fill(
+                padding_mask, min_dtype
+            )
+
+    return causal_mask
+@dataclass
+class CausalLMOutputWithPast(ModelOutput):
+    logit_list : List
+
+class Phi3PreTrainedModel(PreTrainedModel):
+    config_class = NewPhi3Config
+    base_model_prefix = "model"
+    supports_gradient_checkpointing = True
+    _no_split_modules = ["Phi3DecoderLayer"]
+    # _skip_keys_device_placement = "past_key_values"
+    # _supports_flash_attn_2 = True
+    # _supports_sdpa = True
+    # _supports_cache_class = True
+
+
+                
+class EmbedModel(nn.Module):
+    def __init__(self, config: Phi3Config):
+        super().__init__()
+        self.padding_idx = config.pad_token_id
+        self.vocab_size = config.vocab_size
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx).to(config.device)
+        self.embed_dropout = nn.Dropout(config.embd_pdrop)   
+        
+    def load_weights(self):
+        global pre_weight_map, tensor_dict
+        
+        new_state_dict = {}
+        new_state_dict['embed_tokens.weight'] = tensor_dict['model.embed_tokens.weight']
+        
+        self.load_state_dict(new_state_dict)
+        del tensor_dict['model.embed_tokens.weight']
+    
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None
+    ):
+
+        inputs_embeds = self.embed_tokens(input_ids)
+
+        return inputs_embeds
+        
+        
+        
+        
+class Body(Phi3PreTrainedModel):
+
+
+    def __init__(self, block_size, config: NewPhi3Config):
+        super().__init__(config)
+        self.config = config
+        self.layers = nn.ModuleList(
+            [Phi3DecoderLayer(self.config, i) for i in range(block_size)]
+        ).to(self.config.device)
+        self._attn_implementation = self.config._attn_implementation
+        self.gradient_checkpointing = False
+        # Initialize weights and apply final processing
+        self.block_size = block_size
+        
+
+    def load_one_file(self):
+    
+        global file_num, tensor_dict
+        print(f'{file_num}번째 파일 오픈함', flush=True)
+        if file_num > 6:
+            print("파일 번호가 6번을 넘어감")
+        file_path = self.config.base_path + f'/model-0000{file_num}-of-00006.safetensors'
+        
+        with safe_open(file_path, framework="pt", device=self.config.device) as f:
+            for key in f.keys():
+                tensor_dict[key] = f.get_tensor(key)
+        
+        file_num += 1
+
+    def load_weights(self, idx):
+        
+        new_state_dict = {}
+        global pre_weight_map, tensor_dict
+        
+        partial_model_keys = list(self.layers.state_dict().keys())
+        
+        names = []
+        for key in partial_model_keys:
+            try:
+                num = int(key[0:2])
+                pre_num = num + idx
+                pre_name = "model.layers." + str(pre_num) + key[2:]
+            except:
+                num = int(key[0])
+                pre_num = num + idx
+                pre_name = "model.layers." + str(pre_num) + key[1:]
+            try:
+                new_state_dict[key] = tensor_dict[pre_name]
+                names.append(pre_name)
+            except:
+                self.load_one_file()
+                new_state_dict[key] = tensor_dict[pre_name]
+        
+        
+        self.layers.load_state_dict(new_state_dict)
+        
+        for name in names:
+            del tensor_dict[name]
+        del partial_model_keys
+        
+        
+        
+
+
+    def forward(
+        self,
+        idx,
+        hidden_states: torch.LongTensor = None,
+        causal_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+    ):
+        st = time.time()
+        for decoder_layer in self.layers:
+            
+            layer_outputs = decoder_layer(
+                    hidden_states,
+                    attention_mask=causal_mask,
+                    position_ids=position_ids,
+                    past_key_value=past_key_values,
+                    cache_position=cache_position,
+                    output_attentions=None,
+                    use_cache=False
+                )
+
+            hidden_states = layer_outputs[0]
+        end = time.time()
+        print(f'디코더 레이어 한 배치 포워드 {end-st}초', flush=True)
+        return hidden_states
+
+
+class CustomedPhi3ForCausalLM(Phi3PreTrainedModel):
+    _tied_weights_keys = ["lm_head.weight"]
+
+    
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
+        self.norm = Phi3RMSNorm(self.config.hidden_size).to(self.config.device)
+        self.lm_head = nn.Linear(self.config.hidden_size, self.config.vocab_size, bias=False).to(self.config.device)
+        
+
+    def load_weights(self):
+        global pre_weight_map, tensor_dict
+        new_state_dict = {}
+ 
+        new_state_dict['norm.weight'] = tensor_dict['model.norm.weight']
+        new_state_dict['lm_head.weight'] = tensor_dict['lm_head.weight']
+                    
+        self.load_state_dict(new_state_dict)
+        
+        del tensor_dict['model.norm.weight']
+        del tensor_dict['lm_head.weight']
+    
+    def load_one_file(self):
+    
+        global file_num
+        
+        if file_num > 6:
+            print("파일 번호가 6번을 넘어감")
+        file_path = self.config.base_path + f'/model-0000{file_num}-of-00006.safetensors'
+        
+        with safe_open(file_path, framework="pt", device=self.config.device) as f:
+            for key in f.keys():
+                tensor_dict[key] = f.get_tensor(key)
+        
+        file_num += 1
+        
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        attention_mask_list: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[List[torch.FloatTensor]] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        use_cache: Optional[bool] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+    ) -> Union[Tuple, CausalLMOutputWithPast]:
+        
+        global file_num
+        file_num = 1
+        self.load_one_file()
+        embed_model = EmbedModel(self.config)
+        embed_model.load_weights()
+        
+        hidden_tensor = torch.zeros(input_ids.shape[0], input_ids.shape[1], input_ids.shape[2], self.config.hidden_size, dtype=torch.float32, device=self.config.device)
+        for i, inputs in enumerate(input_ids):
+            hidden_tensor[i].copy_(embed_model(inputs))
+        del embed_model
+        
+        max_seq_len = max([hidden_states.shape[1] for hidden_states in hidden_tensor])
+
+        cache_position_tensor = torch.zeros(len(hidden_tensor), max_seq_len, device=self.config.device, dtype=torch.int32)
+        position_ids_tensor = torch.zeros(len(hidden_tensor), 1, max_seq_len, device=self.config.device, dtype=torch.int32)
+        cache_position = torch.arange(0, max_seq_len, device=self.config.device)
+        position_ids = cache_position.unsqueeze(0)
+        
+        for i, hidden_states in enumerate(hidden_tensor):
+            st = time.time()
+            seq_len = hidden_states.shape[1]
+            cache_position_tensor[i, :seq_len] = cache_position[:seq_len]
+            position_ids_tensor[i, :, :seq_len] = position_ids[:, :seq_len]
+            # causal_mask_list.append(_prepare_4d_causal_attention_mask_with_cache_position(
+            #                                 attention_mask=attention_mask_list[i],
+            #                                 sequence_length=input_ids[i].shape[1],
+            #                                 target_length=input_ids[i].shape[1],
+            #                                 dtype=input_ids[i].dtype,
+            #                                 device=input_ids[i].device,
+            #                                 min_dtype = torch.iinfo(torch.int32).min,
+            #                                 cache_position=cache_position_list[i],
+            #                                 batch_size=input_ids[i].shape[0],
+            #                                 ))
+            end = time.time()
+            print(f'cache position ids 저장{end-st}초', flush=True)
+            
+        body = Body(self.config.block_size, self.config)
+        
+        current_stream = torch.cuda.current_stream()
+        stream1 = torch.cuda.Stream()
+
+        for idx in range(0, 40, self.config.block_size):
+            body.load_weights(idx)
+            st = time.time()
+            for i, hidden_states in enumerate(hidden_tensor):
+                with torch.cuda.stream(current_stream):
+                    outputs = body(idx, hidden_states, attention_mask_list[i], position_ids_tensor[i], None, cache_position_tensor[i])
+                    print('output 계산중')
+                    
+                with torch.cuda.stream(stream1):
+                    hidden_tensor[i].copy_(outputs)
+                    print('stream1 이 업데이트 ')
+                
+                del outputs
+            torch.cuda.synchronize()
+            end = time.time()
+            print('싱크 맞추기 ')
+            print(f'배치들 전체 포워드 {end-st}초', flush=True)
+        del body
+
+        self.load_weights()
+        
+        logit_list = torch.zeros(hidden_tensor.shape[0], hidden_tensor.shape[1], hidden_tensor.shape[2], self.config.vocab_size, dtype=torch.float32, device=self.config.device)
+        for hidden_states in hidden_tensor:
+            hidden_states = self.norm(hidden_states)
+            logits = self.lm_head(hidden_states).float()
+            st = time.time()
+            logit_list[i].copy_(logits)
+            end = time.time()
+            print(f'logit list 저장 {end-st}초', flush=True)
+            del logits
+            
+        del hidden_tensor
+
+        print('forward')
+
+        return CausalLMOutputWithPast(
+            logit_list=logit_list
+        )
+
+    # Copied from transformers.models.llama.modeling_llama.LlamaForCausalLM.prepare_inputs_for_generation
+    def prepare_inputs_for_generation(
+        self,
+        input_ids,
+        past_key_values=None,
+        attention_mask=None,
+        inputs_embeds=None,
+        cache_position=None,
+        position_ids=None,
+        use_cache=True,
+        **kwargs,
+    ):
+
+        if attention_mask is not None and position_ids is None:
+            # create position_ids on the fly for batch generation
+            position_ids = attention_mask.long().cumsum(-1) - 1
+            position_ids.masked_fill_(attention_mask == 0, 1)
+            if past_key_values:
+                position_ids = position_ids[:, -input_ids.shape[1] :]
+
+                # This `clone` call is needed to avoid recapturing cuda graphs with `torch.compile`'s  `mode="reduce-overhead`, as otherwise the input `position_ids` would have various stride during the decoding. Here, simply using `.contiguous()` is not sufficient as in the batch size = 1 case, `position_ids` is already contiguous but with varying stride which retriggers a capture.
+                position_ids = position_ids.clone(memory_format=torch.contiguous_format)
+
+        # if `inputs_embeds` are passed, we only want to use them in the 1st generation step
+        if inputs_embeds is not None and cache_position[0] == 0:
+            model_inputs = {"inputs_embeds": inputs_embeds, "input_ids": None}
+        else:
+            # The clone here is for the same reason as for `position_ids`.
+            model_inputs = {"input_ids": input_ids.clone(memory_format=torch.contiguous_format), "inputs_embeds": None}
+
+        
+
+        model_inputs.update(
+            {
+                "position_ids": position_ids,
+                "cache_position": cache_position,
+                "past_key_values": past_key_values,
+                "use_cache": use_cache,
+                "attention_mask": attention_mask,
+            }
+        )
+        return model_inputs
+    
+    def _update_causal_mask(
+        self,
+        attention_mask: torch.Tensor,
+        input_tensor: torch.Tensor,
+        cache_position: torch.Tensor,
+        past_key_values =None,
+        output_attentions= False,
+    ):
+
+        if self.config._attn_implementation == "flash_attention_2":
+            if attention_mask is not None and 0.0 in attention_mask:
+                return attention_mask
+            return None
+
+        
+        past_seen_tokens =  0
+
+        dtype, device = input_tensor.dtype, input_tensor.device
+        min_dtype = torch.finfo(dtype).min
+        sequence_length = input_tensor.shape[1]
+        target_length = (
+                attention_mask.shape[-1]
+            )
+
+        causal_mask = _prepare_4d_causal_attention_mask_with_cache_position(
+            attention_mask,
+            sequence_length=sequence_length,
+            target_length=target_length,
+            dtype=dtype,
+            device=device,
+            min_dtype=min_dtype,
+            cache_position=cache_position,
+            batch_size=input_tensor.shape[0],
+        )
+
+
+        return causal_mask
+    
+    
+
+    
---
+++ pipeline2.py
@@ -0,0 +1,146 @@
+import torch
+from transformers import AutoTokenizer
+import json
+import time
+import gc
+from generation import CustomGeneration
+torch.random.manual_seed(0)
+model_id = "microsoft/Phi-3-medium-4k-instruct"
+# model = AutoModelForCausalLM.from_pretrained(
+#     model_id,
+#     device_map="cuda",
+#     torch_dtype="auto",
+#     trust_remote_code=True,
+# )
+
+
+class CustomedPipeline():
+    def __init__(
+            self,
+            model,
+            config,
+            model_id = "microsoft/Phi-3-medium-4k-instruct",
+            device = "cuda"
+        ):
+        self.config = config
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.model =  model
+        self.input_ids = []
+        self.attention_mask = []
+        self.device = device
+        self.labels = []
+        self.batch_size = 0
+        self.generate_cls =  CustomGeneration(self.model)
+
+    def batchify(self, data, batch_size):
+        return [data[i:i + batch_size] for i in range(0, len(data), batch_size)]
+    
+    def preprocess(self, data):
+        prefix = "\nRead the question and answer the following sentence in given multiple choice.\nAnswer only the sentence you chose. Never include a question and other word in your answer.\n\nquestion: "
+        model_inputs = []
+        for i in range(len(data['question_stem'])):
+            offset = ord(data['answerKey'][i]) - ord('A')
+            chat_dict = {
+                "messages" :[
+                    {
+                        "role" : "user",
+                        "content" : prefix + data['question_stem'][i] + "\nchoices: ["
+                    }
+                ],
+                "answer" : data['choices'][i]['text'][offset]
+            }
+            
+            for j in range(4):
+                chat_dict['messages'][0]['content'] += "\'" + data['choices'][i]['text'][j]
+                if j < 3:
+                    chat_dict['messages'][0]['content'] += "\', "
+                else:
+                    chat_dict['messages'][0]['content'] += "\']\n"
+            
+            model_inputs.append(chat_dict)
+        
+        # return the processed data as a dict
+        return {"processed_data": model_inputs}
+    
+    def load_data(self, file_path, batch_size):
+        model_inputs = []
+        with open(file_path, 'r', encoding='utf-8') as f:
+            for line in f:
+                json_obj = json.loads(line)
+                model_inputs.append(json_obj)
+        messages = [inputs['message'] for inputs in model_inputs]
+        self.labels = [inputs['answer'] for inputs in model_inputs]
+        self.batch_size = batch_size
+        batches = self.batchify(messages, batch_size)
+        tokenized = [self.tokenizer.apply_chat_template(batch, 
+                                                tokenize=True, 
+                                                padding=True, 
+                                                truncation=True,
+                                                return_tensors="pt", 
+                                                return_dict=True) for batch in batches]
+        
+        self.input_ids = [token['input_ids'] for token in tokenized]
+        self.attention_mask = [token['attention_mask'] for token in tokenized]
+        self.prompt_lens = [token['input_ids'].shape[-1] for token in tokenized]
+        
+    def forward(self, max_new_tokens):
+        times = 0
+        result = []
+        cnt = 0
+        for batch in zip(self.input_ids, self.attention_mask):
+            st = time.time()
+            inputs = batch[0].to(self.device)
+            masks = batch[1].to(self.device)
+           
+            generated_sequence = self.generate_cls.generate(input_ids=inputs, attention_mask=masks, max_length=max_new_tokens)
+ 
+            result.append(generated_sequence)
+            end = time.time()
+            print('batch load and inference time ', end - st)
+            print('batch load and inference time per item', (end - st) / (inputs.shape[0]))
+            times += end - st
+            cnt += 1
+            if cnt % 5 == 0:
+                gc.collect()
+            break
+        print('total inference time ', times)
+        return {"generated_text": result}
+
+    def find_pattern(self, text):
+        idx = []
+        for i in range(91,len(text)-1):
+            if text[i] == 32007 and text[i+1]==32001:
+                idx.append(i)
+           
+        if len(idx) == 0:
+            return text[90:]
+        elif len(idx) == 1:
+            return text[idx[0]+2:]
+        else:
+            return text[idx[0]+2: idx[1]]
+
+    def postprocess(self,model_outputs, clean_up_tokenization_spaces=True):
+
+        result = []
+
+        correct = 0
+        print(len(model_outputs['generated_text']))
+        
+        for outputs in model_outputs['generated_text']:
+            tmp_dict = {}
+            for i, text in enumerate(outputs):
+                answer = self.find_pattern(text)
+                prefill = self.tokenizer.decode(text[self.prompt_lens[i//self.batch_size]:])
+                decoded_answer = self.tokenizer.decode(answer)
+                if self.labels[i] in decoded_answer:
+                    correct += 1
+                else:
+                    decoded_answer = self.tokenizer.decode(text[91:])
+                tmp_dict['generated_text'] = [{'content':prefill, 'role' : 'user'},{'role':'assistant', 'content':decoded_answer}]
+                result.append([tmp_dict])
+        total = len(self.labels)
+        print('맞은 개수', correct)
+        print('총 개수 ',len(self.labels))
+
+        print('accuracy : ',correct/total)
+        return result
